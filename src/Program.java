286550383169c0cf28f831ce6f8091ec663609fd
Vavou <leptilemurien@hotmail.com>
2016-02-22 13:44:45 +0100

    Initialization of the project

---
+++ src/Program.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uma.jmetal.solution.IntegerSolution;
+import org.uma.jmetal.solution.Solution;
+
+/**
+ * @author Vavou
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		List<Solution<IntegerSolution>> population = new ArrayList<Solution<IntegerSolution>>();
+
+	}
+
+}
