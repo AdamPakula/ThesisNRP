7dbb67c2c9961905e90e3a84a49424e0b03e273c
Vavou <valentin.elvassore@gmail.com>
2016-04-28 12:37:15 +0200

    Improve the comments

--- src/entities/Skill.java
+++ src/entities/Skill.java
@@ -2,25 +2,41 @@ package entities;
 
 public class Skill {
 	
+	/* --- Atributes --- */
+	
 	/**
 	 * The name of the Skill
 	 */
 	private String name;
 	
+	
+	/* --- Getters and setters --- */
+	
 	/**
-	 * @return the name
+	 * @return the name of the skill
 	 */
 	public String getName() {
 		return name;
 	}
 
 	/**
-	 * @param name the name to set
+	 * Set the name of the skill
+	 * @param name the new name of the skill, can't be null
 	 */
 	public void setName(String name) {
+		if (name == null) {
+			throw new NullPointerException();
+		}
 		this.name = name;
 	}
+	
+	
+	/* --- Constructors --- */
 
+	/**
+	 * Constructs a skill
+	 * @param name the name of the skill to construct
+	 */
 	public Skill(String name) {
 		this.name = name;
 	}
