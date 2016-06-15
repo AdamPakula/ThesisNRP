679738b34f1d3dcc4a20680869110c5868585af0
Vavou <valentin.elvassore@gmail.com>
2016-06-15 17:52:12 +0200

    Rename some parameters

--- ExperimentingNRP/src/parameters/DefaultParameters.java
+++ ExperimentingNRP/src/parameters/DefaultParameters.java
@@ -4,7 +4,7 @@ public class DefaultParameters {
 	
 	/* --- Iteration --- */
 	public final static int NUMBER_OF_WEEK = 3;
-	public final static int HOURS_BY_WEEK = 35;
+	public final static double HOURS_BY_WEEK = 35.0;
 	
 	/* --- Generator --- */
 	public final static int NUMBER_OF_TASKS = 20;
@@ -14,9 +14,9 @@ public class DefaultParameters {
 	
 	
 	/* --- Algorithm --- */
-	public final static int nbIterations = 500;
-	public final static int populationSize = 100;
-	public final static double crossoverProbability = 0.5;
-	public final static double mutationProbability = 1.0/NUMBER_OF_TASKS;
+	public final static int NUMBER_OF_ITERATIONS = 500;
+	public final static int POPULATION_SIZE = 100;
+	public final static double CROSSOVER_PROBABILITY = 0.5;
+	public final static double MUTATION_PROBABILITY = 1.0/NUMBER_OF_TASKS;
 
 }
