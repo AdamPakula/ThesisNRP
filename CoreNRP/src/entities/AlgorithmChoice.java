724e63aa4505994e3679b86026d9b0a61039fe13
Vavou <valentin.elvassore@gmail.com>
2016-06-15 17:32:38 +0200

    Update the algorithms use to the experiment

--- CoreNRP/src/entities/AlgorithmChoice.java
+++ CoreNRP/src/entities/AlgorithmChoice.java
@@ -1,9 +1,15 @@
 package entities;
 
 public enum AlgorithmChoice {
+	//GENERATIONAL("Generational GA"), 
+	MOCell("MOCell"),
 	NSGAII("NSGA II"),
-	MOCELL("MOCell");
-	
+	PESA2("PESA2"),
+	//SMSEMOA("SMSEMOA"), // Does not evaluate the constraints...
+	SPEA2("SPEA2"),
+	//STEADY("Steady State GA") // The evaluator does not consider the constraints...
+	;
+	 
 	private String name;
 	
 	private AlgorithmChoice(String name) {
