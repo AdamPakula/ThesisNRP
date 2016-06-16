795b5f85a27d6f7fc95337a6993ed3c89010be07
Vavou <valentin.elvassore@gmail.com>
2016-06-16 19:13:43 +0200

    Add experiment parameters

--- ExperimentingNRP/src/parameters/DefaultParameters.java
+++ ExperimentingNRP/src/parameters/DefaultParameters.java
@@ -14,9 +14,14 @@ public class DefaultParameters {
 	
 	
 	/* --- Algorithm --- */
-	public final static int NUMBER_OF_ITERATIONS = 500;
+	public final static int NUMBER_OF_ITERATIONS = 250;
 	public final static int POPULATION_SIZE = 100;
 	public final static double CROSSOVER_PROBABILITY = 0.5;
 	public final static double MUTATION_PROBABILITY = 1.0/NUMBER_OF_TASKS;
+	
+	
+	/* Experiment */
+	public final static int TASKS_BY_EMPLOYEE = 5;
+	public final static int MAX_PROBLEM_SIZE = 400;
 
 }
