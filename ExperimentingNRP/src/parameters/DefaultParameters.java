2e0054ee83b3cb3922c620ebfed04531c27601cc
Vavou <valentin.elvassore@gmail.com>
2016-06-21 19:15:56 +0200

    Moving the default generator parameters into a new class

--- ExperimentingNRP/src/parameters/DefaultParameters.java
+++ ExperimentingNRP/src/parameters/DefaultParameters.java
@@ -1,28 +1,23 @@
 package parameters;
 
+import entities.DefaultGeneratorParameters;
+
 public class DefaultParameters {
 	
 	/* --- Iteration --- */
 	public final static int NUMBER_OF_WEEK = 3;
 	public final static double HOURS_BY_WEEK = 35.0;
 	
-	/* --- Generator --- */
-	public final static int NUMBER_OF_TASKS = 20;
-	public final static int NUMBER_OF_EMPLOYEES = 2;
-	public final static int NUMBER_OF_SKILLS = 2;
-	public final static double PRECEDENCE_RATE = 0.1;
-	
-	
 	/* --- Algorithm --- */
-	public final static int NUMBER_OF_ITERATIONS = 250;
+	public final static int NUMBER_OF_ITERATIONS = 300;
 	public final static int POPULATION_SIZE = 100;
 	public final static double CROSSOVER_PROBABILITY = 0.5;
-	public final static double MUTATION_PROBABILITY = 1.0/NUMBER_OF_TASKS;
+	public final static double MUTATION_PROBABILITY = 1.0/DefaultGeneratorParameters.NUMBER_OF_TASKS;
 	
 	
 	/* Experiment */
 	public final static int TASKS_BY_EMPLOYEE = 5;
 	public final static int MAX_PROBLEM_SIZE = 400;
-	public final static int TEST_REPRODUCTION = 10;
+	public final static int TEST_REPRODUCTION = 100;
 
 }
---
+++ GeneratorNRP/src/entities/DefaultGeneratorParameters.java
@@ -0,0 +1,31 @@
+package entities;
+
+
+/**
+ * The defaults values for the generator parameters
+ * @author Vavou
+ *
+ */
+public class DefaultGeneratorParameters {
+
+	/**
+	 * The default number of tasks to generate
+	 */
+	public final static int NUMBER_OF_TASKS = 20;
+	
+	/**
+	 * The default number of employees to generate
+	 */
+	public final static int NUMBER_OF_EMPLOYEES = 2;
+	
+	/**
+	 * The default number of skills to generate
+	 */
+	public final static int NUMBER_OF_SKILLS = 2;
+	
+	/**
+	 * The default precedence rate for the generation
+	 */
+	public final static double PRECEDENCE_RATE = 0.1;
+
+}
