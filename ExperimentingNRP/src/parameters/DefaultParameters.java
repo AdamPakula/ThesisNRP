2b122321c4236af188d0a212882f6f78388d6f51
Vavou <valentin.elvassore@gmail.com>
2016-06-22 17:58:06 +0200

    Remove unused import

--- ExperimentingNRP/src/parameters/DefaultParameters.java
+++ ExperimentingNRP/src/parameters/DefaultParameters.java
@@ -1,14 +1,12 @@
 package parameters;
 
-import entities.DefaultGeneratorParameters;
-
 public class DefaultParameters {
 	
-	/* --- Algorithm --- */
+	/* --- Executor --- */
 	public final static int POPULATION_SIZE = 100;
 	public final static int NUMBER_OF_ITERATIONS = 250;
 	
-	/* Experiment */
+	/* --- Experiment --- */
 	public final static int TASKS_BY_EMPLOYEE = 5;
 	public final static int MAX_PROBLEM_SIZE = 400;
 	public final static int TEST_REPRODUCTION = 10;
